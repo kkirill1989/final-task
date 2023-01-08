0d80121d9a7f807a0fff234b09661c3827e6094a
Kirill Kuznetsov <k.kirill1989@mail.ru>
2023-01-08 15:40:15 +0600

    add task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Наити сумму элементов от M до N, N и M заданы.
+int SumNumbers(int M, int N)
+{
+    if(N == M) return N;
+    return M + SumNumbers(M + 1, N);
+}
+Console.Write("Сумма элементов от M до N: равна ");
+Console.WriteLine(SumNumbers(1, 9));
+
