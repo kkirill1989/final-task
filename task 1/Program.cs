501b14e0ddd8ac2e0ac09daafc8943ea47009537
Kirill Kuznetsov <k.kirill1989@mail.ru>
2023-01-08 15:26:54 +0600

    add task 1

---
+++ task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿//  показать натуральные числа от N до  N, N и M заданы. 
+Console.Write("Покажем натуральные числа от M до N");
+Console.WriteLine();
+Console.Write("Введите натуральное число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите натуральное число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+if (m < n)
+{
+    while (m < n + 1)
+    {
+        Console.Write($"{m} ");
+        m++;
+    }
+}
+else if (m > n)
+{
+    while (m + 1 > n)
+    {
+        Console.Write($"{m} ");
+        m--;
+    }
+}
+else
+{
+    Console.WriteLine();
+    Console.Write($"Вы ввели одинаковые числа, попробуйте еще.");
+}
