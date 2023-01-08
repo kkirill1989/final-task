59191de51d7dcd712c6524c1195ed9b55e873df9
Kirill Kuznetsov <k.kirill1989@mail.ru>
2023-01-08 15:51:20 +0600

    add task 4

---
+++ task 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степень B.
+int PowNum(int A, int B)
+{
+    if (B == 0) return 1;
+    else if (B == 1) return A;
+    return A * PowNum(A, B - 1);
+}
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {A} возведенное в степень {B} равно {PowNum(A, B)}");
+
